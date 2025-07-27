a4da7e5c3ae027d17c0fcc213e00cc0ba5701a54
Greety Guide <greetyguide008@gmail.com>
2025-07-27 13:35:44 +0530

    Todo-Beginning

---
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -0,0 +1,30 @@
+package com.app.todoapp.controller;
+
+import com.app.todoapp.models.Task;
+import com.app.todoapp.services.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    //constructor injection
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model){
+        List<Task> tasks = taskService.getAllTasks();
+        //html template of tasks
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+}
